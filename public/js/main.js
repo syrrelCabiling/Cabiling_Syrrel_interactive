3915aee8af0ab6887b3ef069caaed14b5e25143a
Syrrel Cabiling <syrrelcabiling@Syrrels-MBP.fc.ca>
2019-11-29 16:02:47 -0500

    moved the images inside the scale div

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,42 @@
 const myVEI = (() => {
     // get the user buttons and fire off an async DB query with Fetch
     let indexButtons = document.querySelectorAll('.index-link'), //to trigger each index and reveal data on lightbox
-    lightbox = document.querySelector('.lightbox'); //this is the scale
+    lightbox = document.querySelector('.lightbox'),
+    volcanoDiv = document.querySelector('.vImage'); //this is the scale
 
      function veiIndex(vIndex) { // vIndex is the database result
-        let targetDiv = document.querySelector('.lightbox'),
-        targetImg = lightbox.querySelector('img');
-
+        let targetDiv = document.querySelector('.lightbox');
+       
         let veiContainer = `
-            <img src="${vIndex.Image}" class="vImage">
+           
             <h5>Classification:</h5><p>${vIndex.Classification}</p>
            <h5>Volcano:</h5><p>${vIndex.Volcano}</p>
            <h5>Tephra:</h5><p>${vIndex.Tephra}</p>
            <h5>How Often:</h5><p>${vIndex.HowOften}</p>
            <h5>Death Toll:</h5><p>${vIndex.OtherDamages}</p>
         `;
-        
+
         console.log(veiContainer);
 
         targetDiv.innerHTML = veiContainer;
-        targetImg.src = vIndex.Image;
-   
-        
+
     }
+
+    function fetchVolcanoImg(vIndex) {
+        let volcanoImg = document.querySelector('.vImage'),
+            targetvImg = volcanoDiv.querySelector('img');
+
+        let volImg = `
+            <img src="${vIndex.Image}">
+            `;
+    console.log(volImg);
+
+    volcanoImg.innerHTML = volImg;
+    targetvImg.src = vIndex.Image;
+    
+
+    }
+
     function volcanoData(event) {
         event.preventDefault(); //kill default tag in behaviour (dont navigate anywhere)
        // debugger;
@@ -37,7 +51,8 @@ const myVEI = (() => {
 
            data[0].imgsrc = imgSrc;
 
-           veiIndex(data[0]);
+           veiIndex(data[0]),
+           fetchVolcanoImg(data[0]);
        })
        .catch((err) => {
            console.log(err)
